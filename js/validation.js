579f9bca5cc6e3c504b827d4b72c1b8dd2802387
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2023-02-03 00:01:51 +0100

    Added a hashing function to my validation code

--- js/validation.js
+++ js/validation.js
@@ -1,3 +1,22 @@
+const USERS = {"example@example.com": "0039829837498983749823"}
+
+/* Code from https://www.geeksforgeeks.org/how-to-create-hash-from-string-in-javascript/ */
+function stringToHash(string) {
+             
+    let hash = 0;
+     
+    if (string.length == 0) return hash;
+     
+    for (i = 0; i < string.length; i++) {
+        char = string.charCodeAt(i);
+        hash = ((hash << 5) - hash) + char;
+        hash = hash & hash;
+    }
+     
+    return hash;
+}
+
+
 document.addEventListener("DOMContentLoaded", () => {
     const getEmailaddress = document.getElementById("InputEmail");
     
