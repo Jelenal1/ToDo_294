bf723f98dfc0c0193c17a7892f2a50ce511ce54e
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2023-02-02 13:13:34 +0100

    You can add new todos and they will be displayed and saved into a json object

--- js/index.js
+++ js/index.js
@@ -1,7 +1,23 @@
+function putNewItemsIntoList(list){
+    const listOfToDoItems = document.getElementById("list");
+    listOfToDoItems.innerText = "";
+    if (list == undefined) return;
+    Object.entries(list).forEach((value) => {
+        const newItem = document.createElement("li");
+        newItem.innerText = value[1];
+        listOfToDoItems.append(newItem);
+    });
+}
+
+
 document.addEventListener("DOMContentLoaded", function () {
+    const InputElements = {1: "ToDo1", 2: "ToDo2"};
     const getInputElements = document.getElementById("newtd");
     const getSubmitElements = document.forms["addtodo"];
+    putNewItemsIntoList(InputElements);
+
     getSubmitElements.addEventListener("submit", function (event) {
         event.preventDefault();
+        putNewItemsIntoList(InputElements);
     })
 })
