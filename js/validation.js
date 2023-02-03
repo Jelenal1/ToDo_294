60458095787d86cd8b69c74b3ba7f152457658a7
Jelenal1 <113020871+Jelenal1@users.noreply.github.com>
2023-02-03 08:14:37 +0100

    added rough validation to the signup

--- js/validation.js
+++ js/validation.js
@@ -1,23 +1,28 @@
 const USERS = {"example@example.com": "0039829837498983749823"}
 
-/* Code from https://www.geeksforgeeks.org/how-to-create-hash-from-string-in-javascript/ */
-function stringToHash(string) {
-             
-    let hash = 0;
-     
-    if (string.length == 0) return hash;
-     
-    for (i = 0; i < string.length; i++) {
-        char = string.charCodeAt(i);
-        hash = ((hash << 5) - hash) + char;
-        hash = hash & hash;
-    }
-     
-    return hash;
+function SignUpPasswordIsIdentical(PasswordOne, PasswordTwo){
+     const identicalpassword = PasswordOne.value == PasswordTwo.value;
+     return identicalpassword;
 }
 
 
 document.addEventListener("DOMContentLoaded", () => {
     const getEmailaddress = document.getElementById("InputEmail");
-    
+    const getPassword = document.getElementById("PasswordOne");
+    const getPasswordTwo = document.getElementById("PasswordTwo");
+    const getSubmit = document.getElementById("Signup");
+
+    getSubmit.addEventListener("submit", function (event) {
+        event.preventDefault();
+        console.log("Hello")
+       if (SignUpPasswordIsIdentical(getPassword, getPasswordTwo)) {
+            const newUser = {
+                "email": getEmailaddress.value,
+                "password": getPassword.value
+            }
+            console.log(newUser);
+            console.log(USERS);
+    }
+});
+
 })
